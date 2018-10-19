794605b13b1912023e58e823a23e2f6592eabe1e
gyakkun <xiaowufeixia@gmail.com>
2018-10-19 21:26:23 +0800

    Add CrimeLab class.

---
+++ app/src/main/java/moe/nyamori/criminalintent/CrimeLab.java
@@ -0,0 +1,47 @@
+package moe.nyamori.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private  static  CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context){
+        if(sCrimeLab==null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context contex){
+        mCrimes = new ArrayList<Crime>();
+
+        //For test only
+
+        for(int ctr=0; ctr <100 ; ctr++){
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + ctr);
+            crime.setSolved(ctr%2==0);
+            mCrimes.add(crime);
+        }
+
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for(Crime crime:mCrimes){
+            if(crime.getId().equals(id)){
+                return crime;
+            }
+        }
+        return null;
+    }
+}
