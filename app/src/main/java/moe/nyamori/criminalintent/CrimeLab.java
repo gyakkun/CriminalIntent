76a531ec33b6a2e8815f0c92ec045f4b3790b060
gyakkun <xiaowufeixia@gmail.com>
2018-10-23 21:01:19 +0800

    Start Chapter 14.
    Add a schema.

--- app/src/main/java/moe/nyamori/criminalintent/CrimeLab.java
+++ app/src/main/java/moe/nyamori/criminalintent/CrimeLab.java
@@ -1,15 +1,21 @@
 package moe.nyamori.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabaseCorruptException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import database.CrimeDbSchema.CrimeBaseHelper;
+
 public class CrimeLab {
     private  static  CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab==null) {
@@ -19,6 +25,10 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context){
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
+
         mCrimes = new ArrayList<>();
 
         //For test only
